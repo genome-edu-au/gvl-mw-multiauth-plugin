356e0a500d037511611c80e848fe5b28566cf0ff
loeffler <loeffler@4fd5961b-1777-0410-b7ee-ce31899663aa>
2009-12-01 15:11:32 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/multiauth@16 4fd5961b-1777-0410-b7ee-ce31899663aa

--- tags/v1.3.2/gen_phpdoc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# for utf-8 support edit
-# /usr/share/php/data/PhpDocumentor/phpDocumentor/Converters/HTML/frames/templates/default/templates/header.tpl
-
-phpdoc -pp on -s on -is -dn MultiAuthPlugin -d MultiAuthPlugin/ -t phpdoc
