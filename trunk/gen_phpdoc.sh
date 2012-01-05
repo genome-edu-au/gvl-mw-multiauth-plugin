215e872044298ff96d772b9fcd4d2cf4c4afd149
loeffler <loeffler@4fd5961b-1777-0410-b7ee-ce31899663aa>
2012-01-05 18:00:50 +0000

    * updated the pom file to generate doxygen and phpdoc reports * included configuration file for phpdoc * fixed pom configuration issues with report generation
    git-svn-id: http://svn.berlios.de/svnroot/repos/multiauth@32 4fd5961b-1777-0410-b7ee-ce31899663aa

--- trunk/gen_phpdoc.sh
+++ trunk/gen_phpdoc.sh
@@ -3,4 +3,4 @@
 # for utf-8 support edit
 # /usr/share/php/data/PhpDocumentor/phpDocumentor/Converters/HTML/frames/templates/default/templates/header.tpl
 
-phpdoc -pp on -s on -is -dn MultiAuthPlugin -d MultiAuthPlugin/ -t phpdoc
+phpdoc -pp on -s on -is -dn MultiAuthPlugin -d src/main/php/ -t phpdoc
